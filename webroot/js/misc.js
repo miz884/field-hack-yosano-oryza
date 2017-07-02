431515d379cf682ecaa7b39e2595e1647f8f78cd
Kosuke Mizubayashi <miz@mizba.net>
2017-07-02 23:36:06 +0900

    Add a function to format location descriptions.

--- webroot/js/misc.js
+++ webroot/js/misc.js
@@ -11,3 +11,41 @@ var locationInit = function() {
   });
 };
 
+
+var formatLocation = function(location) {
+  var content = "";
+  // Title.
+  content += "<h2>";
+  content +=  (location.url ? "<a href='" + location.url + "'>" : "");
+  content +=  location.name;
+  content +=  (location.url ? "</a>" : "");
+  content +=  "</h2>";
+  // Incoming.
+  if (location.incoming) {
+    content +=  "<div>";
+    location.incoming.forEach(function(link) {
+      content +=  "ここでは";
+      content +=  LOCATIONS[link.source].name;
+      content +=  "の";
+      content +=  link.product_name;
+      content +=  "を使っています。<br />";
+    });
+    content +=  "</div>";
+  }
+  // Outgoing.
+  if (location.outgoing) {
+    content +=  "<div>";
+    location.outgoing.forEach(function(link) {
+      content +=  LOCATIONS[link.destination].name;
+      content +=  "がここの";
+      content +=  link.product_name;
+      content +=  "を使っています。<br />";
+    });
+    content +=  "</div>";
+  }
+  // Description.
+  content +=  "<div>";
+  content +=  location.desc;
+  content +=  "</div>";
+  return content;
+}
